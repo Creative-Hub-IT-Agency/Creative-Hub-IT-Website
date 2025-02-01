c2c673f702a6271b4e4df7b887e9e0a3255efe3a
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-01 12:16:10 +0600

    animated banner updated

--- src/components/AnimatedBanner.tsx
+++ src/components/AnimatedBanner.tsx
@@ -1,60 +1,58 @@
-import { motion } from "framer-motion";
 import Marquee from 'react-fast-marquee';
 
 const AnimatedBanner = () => {
     return (
-      <div className='relative text-white overflow-hidden mb-16 pt-16 h-80'>
-        {/* Top Section */}
-        {/* 
-    <motion.div
-          className='relative flex whitespace-nowrap md:text-base font-semibold z-10'
-          animate={{ x: ['-100%', '100%'] }}
-          transition={{
-            repeat: Infinity,
-            duration: 10,
-            ease: 'linear',
-          }}
-        ></motion.div>          
-    
-      {/* Scrolling Banner */}
-        <div className=''>
-          <motion.div
-            className='flex whitespace-nowrap md:text-base font-semibold'
-            animate={{ x: ['100%', '-100%'] }}
-            transition={{
-              repeat: Infinity,
-              duration: 10,
-              ease: 'linear',
-            }}
-          ></motion.div>
+      <div className='relative text-white overflow-hidden pt-16 h-96'>
+        {/* first one */}
+        <div style={{ transform: 'rotate(6deg)' }}>
+          <Marquee direction='right'>
+            <div className='flex justify-center bg-[#222222] py-6 font-semibold md:text-base min-w-full'>
+              <span className='mx-4 text-3xl'>DESIGN</span>
+              <span className='mx-4 text-3xl'>+</span>
+              <span className='mx-4 text-3xl'>WEB DEVELOPMENT</span>
+              <span className='mx-4 text-3xl'>+</span>
+              <span className='mx-4 text-3xl'>BRANDING</span>
+              <span className='mx-4 text-3xl'>+</span>
+              <span className='mx-4 text-3xl'>DESIGN</span>
+              <span className='mx-4 text-3xl'>+</span>
+              <span className='mx-4 text-3xl'>WEB DEVELOPMENT</span>
+              <span className='mx-4 text-3xl'>+</span>
+              <span className='mx-4 text-3xl'>BRANDING</span>
+              <span className='mx-4 text-3xl'>+</span>
+              <span className='mx-4 text-3xl'>DESIGN</span>
+              <span className='mx-4 text-3xl'>+</span>
+              <span className='mx-4 text-3xl'>WEB DEVELOPMENT</span>
+              <span className='mx-4 text-3xl'>+</span>
+              <span className='mx-4 text-3xl'>BRANDING</span>
+            </div>
+          </Marquee>
         </div>
 
-      
-        {/* first one */}
-        <Marquee>
-          <div
-            className='flex justify-center bg-[#222222] py-6 font-semibold md:text-base min-w-full'
-            style={{ transform: 'rotate(6deg)' }}
-          >
-            <span className='mx-4 text-3xl'>DESIGN</span>
-            <span className='mx-4 text-3xl'>+</span>
-            <span className='mx-4 text-3xl'>WEB DEVELOPMENT</span>
-            <span className='mx-4 text-3xl'>+</span>
-            <span className='mx-4 text-3xl'>BRANDING</span>
-          </div>
-        </Marquee>
-        <Marquee>
-          <div
-            className='relative overflow-hidden bg-[#E02166] text-white text-center py-6 min-w-full'
-            style={{ transform: 'rotate(-6deg)' }}
-          >
-            <span className='mx-4 text-4xl'>HIRE US NOW</span>
-            <span className='mx-4 text-4xl'>+</span>
-            <span className='mx-4 text-4xl'>HIRE US NOW</span>
-            <span className='mx-4 text-4xl'>+</span>
-            <span className='mx-4 text-4xl'>HIRE US NOW</span>
-          </div>
-        </Marquee>
+        {/* second one */}
+
+        <div className='-mt-[70px]' style={{ transform: 'rotate(-6deg)' }}>
+          <Marquee>
+            <div className='relative overflow-hidden bg-[#E02166] text-white text-center py-6 min-w-full'>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+              <span className='mx-4 text-4xl'>+</span>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+              <span className='mx-4 text-4xl'>+</span>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+              <span className='mx-4 text-4xl'>+</span>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+              <span className='mx-4 text-4xl'>+</span>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+              <span className='mx-4 text-4xl'>+</span>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+              <span className='mx-4 text-4xl'>+</span>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+              <span className='mx-4 text-4xl'>+</span>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+              <span className='mx-4 text-4xl'>+</span>
+              <span className='mx-4 text-4xl'>HIRE US NOW</span>
+            </div>
+          </Marquee>
+        </div>
       </div>
     );
 };
