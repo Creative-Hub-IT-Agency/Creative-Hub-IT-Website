5ea5bfc2d075f50595c84a807a1b5f9c8b9242c0
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-02 15:13:26 +0600

    animated banner updated

--- src/components/portfolioSection/AnimatedBanner.tsx
+++ src/components/portfolioSection/AnimatedBanner.tsx
@@ -2,9 +2,9 @@ import Marquee from 'react-fast-marquee';
 
 const AnimatedBanner = () => {
     return (
-      <div className='relative text-white overflow-hidden mt-20 pt-16 h-96'>
+      <div className='relative text-white overflow-hidden md:mt-20 pt-16 h-96'>
         {/* first one */}
-        <div style={{ transform: 'rotate(6deg)' }}>
+        <div className='rotate-12 md:rotate-[9deg] lg:rotate-6'>
           <Marquee direction='right'>
             <div className='flex justify-center bg-[#222222] py-6 font-semibold md:text-base min-w-full'>
               <span className='mx-4 text-3xl'>DESIGN</span>
@@ -30,7 +30,7 @@ const AnimatedBanner = () => {
 
         {/* second one */}
 
-        <div className='-mt-[70px]' style={{ transform: 'rotate(-6deg)' }}>
+        <div className='-mt-[70px] -rotate-12 md:-rotate-[9deg] lg:-rotate-6'>
           <Marquee>
             <div className='relative overflow-hidden bg-[#E02166] text-white text-center py-6 min-w-full'>
               <span className='mx-4 text-4xl'>HIRE US NOW</span>
